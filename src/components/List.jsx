3af6c32a305b9d1863d6d869126b338a82bd5cd5
gjlee <dlrlwjd56@gmail.com>
2026-03-31 23:19:22 +0900

    0331 todo list

---
+++ src/components/List.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./List.css";
+import TodoItem from "./TodoItem";
+
+const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase()),
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
